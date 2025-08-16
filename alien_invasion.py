7ffab0dbb052b0b1c7f23088cab1d939baf3ee1e
Trajano Bertrand Llera Romero <tlalocantzin00@gmail.com>
2025-08-16 15:54:58 -0600

    Refactorizar código con dos métodos auxilares

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,17 +22,23 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            #Whatch for keborad and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            #Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blime()
+            self._check_events()
+            self._update_screen()
+    
+    def _check_events(self):
+        """Respond to keypress and mouse events."""
 
-            # Make the most recentl draw screen visible
-            pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the now screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     #Make a game instance, and run the game
