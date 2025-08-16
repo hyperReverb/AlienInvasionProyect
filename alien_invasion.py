48741d2a3931391355c8439d35f6e4145ebe0eca
Trajano Bertrand Llera Romero <tlalocantzin00@gmail.com>
2025-08-16 15:48:40 -0600

    Creación de modulos settings.py alien_invasion.oy y ship.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,23 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage tame assets and behavior"""
 
     def __init__(self):
         """Initialize the game, and create game resource"""
+        pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien invasion")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
 
+        pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -19,10 +27,14 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            #Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blime()
+
             # Make the most recentl draw screen visible
             pygame.display.flip()
 
 if __name__ == '__main__':
     #Make a game instance, and run the game
     ai = AlienInvasion()
-    ai.run_game()
+    ai.run_game() 
