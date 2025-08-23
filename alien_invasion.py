5776581701a7a8c0fc8df931c75433493f98d16a
Hyper_Reverb <lleramiguel826@gmail.com>
2025-08-23 10:25:59 -0600

    Se agrego una refactorizacion para quitar "run_game" y colocar metodos auxiliares

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,17 +19,23 @@ class AlienInvasion:
 
     def run_game(self):
         """"Start the main loop for the game"""
-        while True:
+        while True:                      
+            self._check_events()
+            self._update_screen()
             #watch for keyboard and mouse events.
-            for event in pygame.event.get():
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
             #Redraw the secreen during each pass through the loop.
-            self.screen.fill(self.settiings.bg_color)
-            self.ship.blitme()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settiings.bg_color)
+        self.ship.blitme()
             # Make the most recently drawn screen visble.
-            pygame.display.flip()
+        pygame.display.flip()
+
 
 if __name__=='__main__':
     #Make a game instance, and run the game.
